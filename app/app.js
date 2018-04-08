47a441c142b5357fa10a6b8f8e5c206af5d8f106
MrHZJ <web_zhijun@163.com>
2018-04-08 10:04:17 +0800

    第四步：实现分页请求功能

--- app/app.js
+++ app/app.js
@@ -9,10 +9,18 @@
 		//'movie.top250'
 	])
 	.config(['$routeProvider', function($routeProvider) {
-		$routeProvider.otherwise({
+		$routeProvider.when('/:type/:page', {
+			templateUrl: 'controller/loadPage.html',//路径是以index.html为基础，因为模板放在上面
+			controller: 'pageControllers'
+		}).otherwise({
 			redirectTo: '/hot/1'
 		});
 	}])
+	//.config(['$routeProvider', function($routeProvider) {
+	//	$routeProvider.otherwise({
+	//		redirectTo: '/hot/1'
+	//	});
+	//}])
 	.directive('autoFocus',[function(){
 
 	}]);
--- app/controller/page.js
+++ app/controller/page.js
@@ -6,14 +6,14 @@
 
 	var pageController = angular.module('loagPage.controller',['ngRoute','loadPage.service']);
 
-	pageController.config(['$routeProvider', function($routeProvider) {
-		$routeProvider.when('/:type/:page', {
-			templateUrl: 'controller/loadPage.html',//路径是以index.html为基础，因为模板放在上面
-			controller: 'pageControllers'
-		});
-	}]);
+	//pageController.config(['$routeProvider', function($routeProvider) {
+	//	$routeProvider.when('/:type/:page', {
+	//		templateUrl: 'controller/loadPage.html',//路径是以index.html为基础，因为模板放在上面
+	//		controller: 'pageControllers'
+	//	});
+	//}]);
 
-	pageController.controller('pageControllers',["$scope",'$routeParams','pageServices','$route',function($scope,$routeParams,pageServices,$route){
+	pageController.controller('pageControllers',["$scope",'$routeParams','pageServices','$route','$location',function($scope,$routeParams,pageServices,$route,$location){
 		console.log($routeParams);
 		//$scope.text = $routeParams.type;
 
@@ -37,61 +37,72 @@
 				cbVal : 'cb',
 				callback : cbFn
 			};
-
 			return parames;
 		}
 
-		$scope.$http = pageServices.$http(
-			ajax(0,
-				 $scope.count,
-				function(res){
-					//console.log(JSON.stringify(res));
-					var res = res;
-					$scope.total = res.total;
-					$scope.title = res.title;
-					$scope.totalPage = Math.ceil(res.total / $scope.count);
-					$scope.data = res.subjects;
-					$scope.$apply();
-		}));
+		$scope.$location = $location;
+		$scope.$watch('$location.path()',function(now){
+			$scope.page = $routeParams.page;
+			$scope.$http = pageServices.$http(
+				ajax((($routeParams.page - 1) * $scope.count),
+					$scope.count,
+					function(res){
+						//console.log(JSON.stringify(res));
+						var res = res;
+						$scope.total = res.total;
+						$scope.title = res.title;
+						$scope.totalPage = Math.ceil(res.total / $scope.count);
+						$scope.data = res.subjects;
+						$scope.$apply();
+					}));
+		});
 
-		//$scope.checkPage = function(num){
-		//	var page = $routeParams.page;
-		//	var type = $routeParams.type;
-		//	var num = num;
-        //
-		//	if(num){//next
-		//		page++;
-		//		if(page > $scope.totalPage){
-		//			page = $scope.totalPage;
-		//		}
-		//		$route.updateParams({page : page,type:type});
-		//	}else {//0 prev
-		//		page--;
-		//		if(page <= 1){
-		//			page = 1;
-		//		}
-		//		$route.updateParams({page : page,type:type});
-		//	}
-        //
-        //
-		//	pageServices.$http(
-		//		ajax(((page - 1) * $scope.count),
-		//			$scope.count,
-		//			function(res){
-		//				//0 -->prev 1--->next
-        //
-		//				//console.log(num);
-		//				console.log('页数 ：' + page);
-		//				console.log('start : ' +((page - 1) * $scope.count));
-		//				$scope.page = page;
-		//				var res = res;
-		//				$scope.total = res.total;
-		//				$scope.title = res.title;
-		//				$scope.data = res.subjects;
-		//				$scope.$apply();
-		//			}
-		//		))
-		//}
+
+		$scope.checkPage = function(num){
+			var page = $routeParams.page;
+			var type = $routeParams.type;
+			var num = num;
+
+			if(num){//next
+				page++;
+				if(page > $scope.totalPage){
+					page = $scope.totalPage;
+				}
+				$route.updateParams({page : page,type:type});
+			}else {//0 prev
+				page--;
+				if(page <= 1){
+					page = 1;
+				}
+				$route.updateParams({page : page,type:type});
+			}
+			//pageServices.$http(
+			//	ajax(((page - 1) * $scope.count),
+			//		$scope.count,
+			//		function(res){
+			//			//0 -->prev 1--->next
+            //
+			//			//console.log(num);
+			//			console.log('页数 ：' + page);
+			//			console.log('start : ' +((page - 1) * $scope.count));
+			//			$scope.page = page;
+			//			var res = res;
+			//			$scope.total = res.total;
+			//			$scope.title = res.title;
+			//			$scope.data = res.subjects;
+			//			$scope.$apply();
+			//		}
+			//	))
+		};
+
+		//num-btn
+		$scope.numBtn = [1,2,3,4,5];
+		$scope.pageNum = function(pageNum){
+			console.log(pageNum);
+			var type = $routeParams.type;
+
+			$route.updateParams({page : pageNum ,type:type});
+		}
 
 	}]);
 
